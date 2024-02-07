9465bcf68c2fcd87cc37d114b3c65044a9a39a43
DmitryWeht <vet.dima@icloud.com>
2024-02-07 15:01:38 +0100

    test5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <>
-    <h1>Hello World!</h1>  
+      <h1>Hello World!</h1>
+      <h2>Dima</h2>
+      <h3>Dimaaaaaa</h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
