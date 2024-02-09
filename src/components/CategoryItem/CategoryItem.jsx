7e4603b33a4323d8bbea867721ab7e76760759c9
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-08 17:58:27 -0800

    test

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -5,6 +5,9 @@ export const CategoryItem = ({ title, image }) => {
   const imageUrl = `${baseUrl}${image}`;
   return (
     <div className={classes.card}>
+      <h2>
+        <Liliia></Liliia>
+      </h2>
       <img src={imageUrl} alt={title} />
       <p>{title}</p>
     </div>
