1a5b6b401c8baca5a2e26c5709a6fd1f020c8f47
Allahverdiyeva Shafayat <allahverdiyevashafayat@outlook.com>
2024-02-11 12:20:51 +0000

    Added component Sale in  MainPage component

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sale from "../components/Sale/Sale";
+
+const MainPage = () => {
+  return (
+    <main>
+      <Sale />
+    </main>
+  );
+};
+
+export default MainPage;
