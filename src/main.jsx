02d0183a62034171f61609fb742b1c2a49bcf1a5
Allahverdiyeva Shafayat <allahverdiyevashafayat@outlook.com>
2024-02-11 11:04:28 +0000

    Added Redux Provider for store

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 import { store } from "./store/store.js";
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainPage />,
+//     // errorElement: <ErrorPage />,
+//     children: [],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
