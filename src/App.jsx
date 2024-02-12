3898057d96538de2346ef6c3c8be5cc7f484ef3e
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-12 08:30:47 -0800

    change ProductList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./layout/header/Header";
+// import Header from "./layout/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <Outlet />
     </div>
   );
