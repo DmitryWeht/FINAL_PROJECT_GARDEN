862ebc2338adc168cb9c0e23d8d5d9fe9d59ab6b
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-13 12:38:33 -0800

    request for a form using RTK Query

--- src/pages/AllProductsPage/AllProductsPage.jsx
+++ src/pages/AllProductsPage/AllProductsPage.jsx
@@ -1,25 +1,5 @@
-import { Link } from "react-router-dom";
-import ProductsList from "../../components/ProductsList/ProductsList";
-import classes from "./AllProductsPage.module.css";
-
 const AllProductsPage = () => {
-  return (
-    <div>
-      <div className="container">
-        <div className={classes.button_box}>
-          <Link to="/">
-            <button>Main page</button>
-          </Link>
-          <div className={classes.line}></div>
-          <button>All products</button>
-        </div>
-        <p className={classes.title}>All products</p>
-        <div>
-          <ProductsList />
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default AllProductsPage;
--- src/pages/SalePage/SalePage.jsx
+++ src/pages/SalePage/SalePage.jsx
@@ -1,24 +1,5 @@
-import { Link } from "react-router-dom";
-
-import ProductsList from "../../components/ProductsList/ProductsList";
-import classes from "./SalePage.module.css";
-
 const SalePage = () => {
-  return (
-    <div>
-      <div className="container">
-        <div className={classes.button_box}>
-          <Link to="/">
-            <button>Main page</button>
-          </Link>
-          <div className={classes.line}></div>
-          <button>All sales</button>
-        </div>
-        <p className={classes.title}>Discounted items</p>
-        <ProductsList content="sale" />
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default SalePage;
