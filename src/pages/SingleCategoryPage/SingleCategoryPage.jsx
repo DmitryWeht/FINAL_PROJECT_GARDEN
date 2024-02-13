0b89d1383488c7cadb5055555c39e0c82e8ac04e
Allahverdiyeva Shafayat <allahverdiyevashafayat@outlook.com>
2024-02-13 15:37:17 +0000

    new komponent

--- src/pages/SingleCategoryPage/SingleCategoryPage.jsx
+++ src/pages/SingleCategoryPage/SingleCategoryPage.jsx
@@ -1,13 +1,13 @@
-import CategoryNavigation from "../../components/CategoryNavigation/CategoryNavigation";
-import ProductsFromCategory from "../../components/ProductsFromCategory/ProductsFromCategory";
+// import CategoryNavigation from "../../components/CategoryNavigation/CategoryNavigation";
+// import ProductsFromCategory from "../../components/ProductsFromCategory/ProductsFromCategory";
 
-const SingleCategoryPage = () => {
-  return (
-    <div>
-      <CategoryNavigation />
-      <ProductsFromCategory />
-    </div>
-  );
-};
+// const SingleCategoryPage = () => {
+//   return (
+//     <div>
+//       <CategoryNavigation />
+//       <ProductsFromCategory />
+//     </div>
+//   );
+// };
 
-export default SingleCategoryPage;
+// export default SingleCategoryPage;
