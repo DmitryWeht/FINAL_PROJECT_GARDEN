64fba41bf4f7ec4bec4d9f8462987f19a284b8f8
Allahverdiyeva Shafayat <allahverdiyevashafayat@outlook.com>
2024-02-13 00:57:26 +0000

    Added the ProductsFromCategory component to display products from the selected category.
    This component allows displaying a list of products from the category selected by the user. It utilizes the useGetCategoryByIdQuery and useGetAllProductsQuery hooks to fetch data about the category and products respectively.

---
+++ src/components/ProductsFromCategory/ProductsFromCategory.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import {
+  useGetAllProductsQuery,
+  useGetCategoryByIdQuery,
+} from "../../store/apiSlice";
+import ProductItem from "../ProductItem/ProductItem";
+
+const ProductsFromCategory = () => {
+  const { id } = useParams();
+  const {
+    data: category,
+    error: categoryError,
+    isLoading: categoryLoading,
+  } = useGetCategoryByIdQuery(id);
+  const {
+    data: products,
+    error: productsError,
+    isLoading: productsLoading,
+  } = useGetAllProductsQuery();
+
+  if (categoryLoading || productsLoading) return <div>Loading...</div>;
+  if (categoryError || productsError)
+    return <div>Error: {categoryError || productsError}</div>;
+
+  return (
+    <div>
+      <div
+      //   className={classes.products_wrapper}
+      >
+        {products &&
+          products.map((product) => (
+            <div key={product.id}>
+              <Link to={`/product/${product.id}`}>
+                <ProductItem product={product} />
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsFromCategory;
