acf4d07df01eb9c8745cc37629419ce7af789ae8
DmitryWeht <vet.dima@icloud.com>
2024-02-15 18:15:28 +0100

    реализация второй страницы categories

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,5 +1,24 @@
-const CategoriesPage = () => {
-  return <div></div>;
+import React from "react";
+import { Link } from "react-router-dom";
+import { CategoriesList } from "../../Components/CategoriesList/CategoriesList";
+import classes from "./CategoriesPage.module.css";
+
+export const CategoriesPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className={classes.button_box}>
+          <Link to="/">
+            <button>Main page</button>
+          </Link>
+          <div className={classes.line}></div>
+          <button>Categories</button>
+        </div>
+        <p className={classes.title}>Categories</p>
+        <CategoriesList listLength={5} />
+      </div>
+    </div>
+  );
 };
 
 export default CategoriesPage;
