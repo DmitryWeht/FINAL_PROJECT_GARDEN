fb7b89d0f6021629297c21f2147af6f5af003d98
Allahverdiyeva Shafayat <allahverdiyevashafayat@outlook.com>
2024-02-16 11:32:55 +0000

    Fixed some errors in the ProductsFromCategory component

--- src/components/ProductsFromCategory/ProductsFromCategory.jsx
+++ src/components/ProductsFromCategory/ProductsFromCategory.jsx
@@ -1,34 +1,22 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import {
-  useGetAllProductsQuery,
-  useGetCategoryByIdQuery,
-} from "../../store/apiSlice";
+import { useGetCategoryByIdQuery } from "../../store/apiSlice";
 import ProductItem from "../ProductItem/ProductItem";
 import classes from "./ProductsFromCategory.module.css";
 
 const ProductsFromCategory = () => {
   const { id } = useParams();
-  const {
-    data: category,
-    error: categoryError,
-    isLoading: categoryLoading,
-  } = useGetCategoryByIdQuery(id);
-  const {
-    data: products,
-    error: productsError,
-    isLoading: productsLoading,
-  } = useGetAllProductsQuery();
+  const result = useGetCategoryByIdQuery(id);
 
-  if (categoryLoading || productsLoading) return <div>Loading...</div>;
-  if (categoryError || productsError)
-    return <div>Error: {categoryError || productsError}</div>;
+  if (!result || !result.data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <div className={classes.products_wrapper}>
-        {products &&
-          products.map((product) => (
+        {result.data.data &&
+          result.data.data.map((product) => (
             <div key={product.id}>
               <Link to={`/products/${product.id}`}>
                 <ProductItem {...product} />
