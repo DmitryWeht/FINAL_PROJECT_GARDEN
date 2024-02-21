c2f4238c0243107477013204637fa7c68f77da7d
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-21 13:25:09 -0800

    added styles for CartForm

--- src/components/CartForm/CartForm.jsx
+++ src/components/CartForm/CartForm.jsx
@@ -3,6 +3,14 @@ import classes from "./CartForm.module.css";
 const CartForm = () => {
   return (
     <div className={classes.form_conteiner}>
+      <div className={classes.text_box}>
+        <h2>Order details</h2>
+        <p>3 items</p>
+        <div className={classes.total_box}>
+          <p>Total</p>
+          <p className={classes.total_sum}>$541,00</p>
+        </div>
+      </div>
       <UserDataForm
         inputStyles={classes.cartInput}
         formStyles={classes.cartForm}
