01cdd18c4b27896b43801c89b5946464dc0871b8
Nadiia Olkhovska <nadusharik@ukr.net>
2024-02-21 12:19:09 +0100

    change eslintrc.cjs

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,17 @@
+import CartForm from "../../components/CartForm/CartForm";
 import { CategoriesSection } from "../../components/CategoriesSection/CategoriesSection";
-import Discount from "../../components/Discount/Discount";
 import { Form } from "../../components/Form/Form";
 import Sale from "../../components/Sale/Sale";
+import SaleBanner from "../../components/SaleBanner/SaleBanner";
 
 export const MainPage = () => {
   return (
     <div>
-      <Discount />
+      <SaleBanner />
       <CategoriesSection />
       <Form />
       <Sale />
+      <CartForm />
     </div>
   );
 };
