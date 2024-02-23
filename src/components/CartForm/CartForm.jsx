b3b564bc7b0aa0183f8e68699beda740e0290da4
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-22 20:19:36 -0800

    added totalAmount and totalQuantity

--- src/components/CartForm/CartForm.jsx
+++ src/components/CartForm/CartForm.jsx
@@ -3,20 +3,18 @@ import { UserDataForm } from "../UserDataForm/UserDataForm";
 import classes from "./CartForm.module.css";
 
 const CartForm = () => {
-  const cartTotalQuantity = useSelector(
-    (state) => state.cart.cartTotalQuantity
-  );
+  const totalQuantity = useSelector((state) => state.cart.cartTotalQuantity);
 
-  const TotalAmount = useSelector((state) => state.cart.cartTotalAmount);
+  const totalAmount = useSelector((state) => state.cart.cartTotalAmount);
 
   return (
     <div className={classes.form_conteiner}>
       <div className={classes.text_box}>
         <h2>Order details</h2>
-        <p>{cartTotalQuantity} items</p>
+        <p>{totalQuantity} items</p>
         <div className={classes.total_box}>
           <p>Total</p>
-          <p className={classes.total_sum}>${TotalAmount}</p>
+          <p className={classes.total_sum}>${totalAmount}</p>
         </div>
       </div>
       <UserDataForm
