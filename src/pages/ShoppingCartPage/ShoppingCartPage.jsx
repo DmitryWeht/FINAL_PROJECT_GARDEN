89ffe8563e0dd18593de30c380aee4287f37e40e
Nadiia Olkhovska <nadusharik@ukr.net>
2024-02-23 08:42:35 +0100

    empty basket

--- src/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -2,8 +2,29 @@ import CartForm from "../../components/CartForm/CartForm";
 import { CartList } from "../../components/CartList/CartList";
 import TitleBar from "../../components/TitleBar/TitleBar";
 import classes from "./ShoppingCartPage.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getTotals } from "../../store/cartSlice";
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
 
 const ShoppingCartPage = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const isCartNotEmpty = cartItems.length > 0
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        dispatch(getTotals());
+      } catch (error) {
+        console.error("Ошибка при загрузке данных о корзине:", error);
+      }
+    };
+    fetchData();
+  }, [dispatch]);
+
   return (
     <div className="container">
 
@@ -12,14 +33,24 @@ const ShoppingCartPage = () => {
           buttonText="Back to the store"
           linkTo="/products"
         />
-        <div className={classes.shopping_basket}>
-          <div>
-             <CartList />
-          </div>
-          <CartForm />
+        <div className={`container ${isCartNotEmpty ? classes.shopping_basket 
+          : classes.empty_basket}`}>
+  
+          {isCartNotEmpty ? (
+            <CartList />
+          ) : (
+            <p>Looks like you have no items in your basket currently.</p>
+          )}
+          <button className={classes.shopping_button}>
+          <NavLink to="/categories">Continue Shopping</NavLink></button> 
+         
         </div>
 
-    </div>
+          {isCartNotEmpty && <CartForm />}
+      
+        </div>
+
+    
   );
 };
 
