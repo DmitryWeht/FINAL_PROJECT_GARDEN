c817f0b03896ecd5c716076a9dc5c6122ee5a47f
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-25 21:59:41 -0800

    adding data from LikedProductPage to Local Storage

--- src/store/likedProductsSlice.js
+++ src/store/likedProductsSlice.js
@@ -1,13 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    likedProducts: [],
-    likedIcons: [],
-    likeTotalQuantity: null,
-}
+    likedProducts: localStorage.getItem("likedProducts")
+        ? JSON.parse(localStorage.getItem("likedProducts"))
+        : [],
+    likeTotalQuantity: localStorage.getItem("likeTotalQuantity")
+        ? parseInt(localStorage.getItem("likeTotalQuantity"))
+        : null,
+};
 
 const likedProductsSlice = createSlice({
-    name: 'likedProducts',
+    name: "likedProducts",
     initialState,
     reducers: {
         addToLikedProducts(state, action) {
@@ -23,11 +26,23 @@ const likedProductsSlice = createSlice({
                 state.likedProducts.push(tempProduct);
                 state.likeTotalQuantity += 1;
             }
+            localStorage.setItem(
+                "likedProducts",
+                JSON.stringify(state.likedProducts)
+            );
+            localStorage.setItem("likeTotalQuantity", state.likeTotalQuantity);
         },
         deleteFromLikedProducts(state, action) {
             const productIdToRemove = action.payload;
-            state.likedProducts = state.likedProducts.filter(item => item.id !== productIdToRemove);
+            state.likedProducts = state.likedProducts.filter(
+                (item) => item.id !== productIdToRemove
+            );
             state.likeTotalQuantity--;
+            localStorage.setItem(
+                "likedProducts",
+                JSON.stringify(state.likedProducts)
+            );
+            localStorage.setItem("likeTotalQuantity", state.likeTotalQuantity);
         },
         getQuantity(state) {
             const { likedProducts } = state;
@@ -40,11 +55,11 @@ const likedProductsSlice = createSlice({
 
             const quantity = uniqueIds.length;
             state.likeTotalQuantity = quantity;
+            localStorage.setItem("likeTotalQuantity", quantity);
         },
-
     },
+});
 
-})
-
-export const { addToLikedProducts, deleteFromLikedProducts, getQuantity } = likedProductsSlice.actions;
+export const { addToLikedProducts, deleteFromLikedProducts, getQuantity } =
+    likedProductsSlice.actions;
 export default likedProductsSlice.reducer;
