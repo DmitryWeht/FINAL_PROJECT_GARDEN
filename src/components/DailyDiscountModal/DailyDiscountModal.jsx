665aa8d04fe0178464e234de307d1b36d3bfe898
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-27 14:57:23 -0800

    created the ButtonDailyDiscount and DailyDiscountModal components

---
+++ src/components/DailyDiscountModal/DailyDiscountModal.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import ProductItem from "../../components/ProductItem/ProductItem";
+import closeImage from "../../media/close.svg";
+import classes from "./DailyDiscountModal.module.css";
+const DailyDiscountModal = ({ isOpen, setIsOpen, product }) => {
+  useEffect(() => {
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+    document.documentElement.style.paddingRight = `${scrollbarWidth}px`;
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.documentElement.style.paddingRight = "";
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  return (
+    <div className={classes.modal}>
+      <div className={classes.card}>
+        <div className={classes.text_box}>
+          <h2>50% discount on product of the day!</h2>
+          <div onClick={() => setIsOpen(false)} className={classes.close_box}>
+            <img src={closeImage} alt="close" />
+          </div>
+        </div>
+        <ProductItem {...product} />
+      </div>
+    </div>
+  );
+};
+
+export default DailyDiscountModal;
