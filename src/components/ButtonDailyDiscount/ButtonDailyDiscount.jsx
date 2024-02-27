b38cba9f87a3f558da32a3b8c59fa01009944240
Liliia Pavlova <pavloval4449@gmail.com>
2024-02-26 23:16:26 -0800

    created ButtonDailyDiscount + styles

---
+++ src/components/ButtonDailyDiscount/ButtonDailyDiscount.jsx
@@ -0,0 +1,11 @@
+import classes from "./ButtonDailyDiscount.module.css";
+
+const ButtonDailyDiscount = () => {
+  return (
+    <div>
+      <button className={classes.button_discount}>1 day discount!</button>
+    </div>
+  );
+};
+
+export default ButtonDailyDiscount;
--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,63 +1,68 @@
 import { NavLink } from "react-router-dom";
+import ButtonDailyDiscount from "../ButtonDailyDiscount/ButtonDailyDiscount";
 import classes from "./NavMenu.module.css";
 
 const NavMenu = ({ nav }) => {
-
   return (
-    <div className={classes.menu}>
-       <div className={classes.menu}>
-            <ul className={`${classes.nav_list} ${nav ? classes.active : ""}`}>
-              <li className={classes.nav_list_item}>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${classes.link} ${classes.active_link}`
-                      : `${classes.link}  : ""}`
-                  }
-                >
-                  Main Page
-                </NavLink>
-              </li>
-              <li className={classes.nav_list_item}>
-                <NavLink
-                  to="/categories"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${classes.link} ${classes.active_link}`
-                      : `${classes.link}  : ""}`
-                  }
-                >
-                  Categories
-                </NavLink>
-              </li>
-              <li className={classes.nav_list_item}>
-                <NavLink
-                  to="/products"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${classes.link} ${classes.active_link}`
-                      : `${classes.link}  : ""}`
-                  }
-                >
-                  All products
-                </NavLink>
-              </li>
-              <li className={classes.nav_list_item}>
-                <NavLink
-                  to="/sales"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${classes.link} ${classes.active_link}`
-                      : `${classes.link}  : ""}`
-                  }
-                >
-                  All sales
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-
+    <div className={classes.nav_wrapper}>
+      <div className={classes.menu}>
+        <ul className={`${classes.nav_list} ${nav ? classes.active : ""}`}>
+          <li className={classes.nav_list_item}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? `${classes.link} ${classes.active_link}`
+                  : `${classes.link}  : ""}`
+              }
+            >
+              Main Page
+            </NavLink>
+          </li>
+          <li className={classes.nav_list_item}>
+            <NavLink
+              to="/categories"
+              className={({ isActive }) =>
+                isActive
+                  ? `${classes.link} ${classes.active_link}`
+                  : `${classes.link}  : ""}`
+              }
+            >
+              Categories
+            </NavLink>
+          </li>
+          <li className={classes.nav_list_item}>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive
+                  ? `${classes.link} ${classes.active_link}`
+                  : `${classes.link}  : ""}`
+              }
+            >
+              All products
+            </NavLink>
+          </li>
+          <li className={classes.nav_list_item}>
+            <NavLink
+              to="/sales"
+              className={({ isActive }) =>
+                isActive
+                  ? `${classes.link} ${classes.active_link}`
+                  : `${classes.link}  : ""}`
+              }
+            >
+              All sales
+            </NavLink>
+          </li>
+          <li className={classes.button}>
+            <ButtonDailyDiscount />
+          </li>
+        </ul>
+      </div>
+      <div className={classes.hidden_button}>
+        <ButtonDailyDiscount />
+      </div>
     </div>
   );
 };
