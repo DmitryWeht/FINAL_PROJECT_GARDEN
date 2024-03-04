8b76308cc95117f4cc803e23a0479cf19512526e
Liliia Pavlova <pavloval4449@gmail.com>
2024-03-03 18:33:14 -0800

    created modal window for DiscountForm

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { CategoriesSection } from "../../components/CategoriesSection/CategoriesSection";
 import { DiscountForm } from "../../components/DiscountForm/DiscountForm";
 import Sale from "../../components/Sale/Sale";
 import SaleBanner from "../../components/SaleBanner/SaleBanner";
+import SubmitModal from "../../components/SubmitModal/SubmitModal";
 
 export const MainPage = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
   return (
     <div>
       <SaleBanner />
       <CategoriesSection />
-      <DiscountForm />
+      <DiscountForm handleOpenModal={handleOpenModal} />
       <Sale />
+      <SubmitModal
+        content="form"
+        handleCloseModal={handleCloseModal}
+        open={openModal}
+      />
     </div>
   );
 };
