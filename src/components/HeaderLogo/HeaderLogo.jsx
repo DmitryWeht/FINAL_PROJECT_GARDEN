2f6025cb6a2956935e5f464c7977b4a785bc3b44
DmitryWeht <vet.dima@icloud.com>
2024-03-11 11:42:02 +0100

    fix dark mode and rename ThemeToggle

--- src/components/HeaderLogo/HeaderLogo.jsx
+++ src/components/HeaderLogo/HeaderLogo.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../media/logo-garden.svg";
 import classes from "./HeaderLogo.module.css";
-import ThemaToggle from "../../components/ThemaToggle/ThemaToggle";
+import ThemaToggle from "../../components/ThemeToggle/ThemeToggle";
 
 const HeaderLogo = () => {
   return (
--- src/store/themeSlice.js
+++ src/store/themeSlice.js
@@ -1,19 +1,28 @@
+// Импортируем функцию создания слайса из Redux Toolkit
 import { createSlice } from "@reduxjs/toolkit";
 
+// Определяем начальное состояние слайса.
+// Используем значение 'light' или 'dark', сохраненное в localStorage, с фолбэком на 'dark', если значение отсутствует
 const initialState = {
   theme: localStorage.getItem("light") || "dark",
 };
 
+// Создаем слайс состояния для темы приложения
 const themeSlice = createSlice({
-  name: "theme",
-  initialState,
+  name: "theme", // Указываем название слайса
+  initialState, // Инициализируем слайс с начальным состоянием
   reducers: {
+    // Определяем редьюсеры для обновления состояния
+    // Редьюсер для переключения темы между 'light' и 'dark'
     toggleTheme: (state) => {
-      (state.theme = state.theme === "light" ? "dark" : "light"),
-        localStorage.setItem("theme", state.theme);
+      // Обновляем состояние темы на противоположное и сохраняем новое значение в localStorage
+      state.theme = state.theme === "light" ? "dark" : "light";
+      localStorage.setItem("theme", state.theme);
     },
   },
 });
 
+// Экспортируем действие toggleTheme для использования в компонентах
 export const { toggleTheme } = themeSlice.actions;
+// Экспортируем редьюсер для интеграции с хранилищем Redux
 export default themeSlice.reducer;
